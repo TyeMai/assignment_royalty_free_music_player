25fd01a0329b42f863b0d0808d50a9fbe786b751
tyemai <tyeers@gmail.com>
2018-03-22 20:07:55 -0700

    did the buttons

--- royal_free.js
+++ royal_free.js
@@ -1,46 +1,70 @@
 var song;
+var $song;
 
 function playSong() {
   song.play()
-}
+};
 
 function pauseSong() {
   song.pause()
-}
-
-
-
+};
 
 $(document).ready( () => {
   $('h4').click(function (event){
+      // to pause any song that is already play8ing so 2 songs dont play at once.
+    if(song !== undefined) {
+      pauseSong()
+    }
+    if(!$(event.target).hasClass("active")) {
+      //start with non highlighting so everything not active.
+      $(event.target).addClass('active')
+      song = $(event.target).next()[0];
+      $("h4").not(this).removeClass('active')
+      playSong()
+      console.log(song)
+    } else { //for
+      $(event.target).removeClass('active')
+      pauseSong()
+    }
+    //to change the artist name with each click
+    $("#artistName").html($(event.target).prev().html())
+    //to change the song title with each click
+    $("#songTitle").html(event.target.innerHTML)
+  });
 
-    // to pause any song that is already play8ing so 2 songs dont play at once.
-  if(song !== undefined) {
-    pauseSong()
-  }
 
-  if($(event.target).hasClass("active") === false) {
-    $(event.target).addClass('active')
-    song = $(event.target).next()[0];
-    $("h4").not(this).removeClass('active')
+  $('#play').click(function() {
+    //$song.prev()addClass('active')
     playSong()
-    console.log(song)
-  } else {
-    $(event.target).removeClass('active')
+  });
+
+  $('#pause').click(function() {
+    //$('h4').removeClass('active')
     pauseSong()
+  });
 
+  $('#back').click(function() {
+    pauseSong() // can use song.play directoly
+    var $songList = $('audio')
+    $song = $(song)
+    song = $songList.eq($songList.index($song) - 1)[0]
+    playSong()
+  })
 
+  $('#forward').click(function() {
+    pauseSong()
+    var $songList = $('audio')
+    $song = $(song)
+    if($songList.index($song) === 4) {
+    song = $songList.eq($songList.index($song) - 4)[0]
+    } else {
+    song = $songList.eq($songList.index($song) + 1)[0]
+    }
+    playSong()
+  })
 
-  }
+  //$('button').css("background-color", "#edf1f7")
 
-  //to change the artist name with each click
-  $("#artistName").html($(event.target).prev().html())
-  //to change the song title with each click
-  $("#songTitle").html(event.target.innerHTML)
-
-
-
-})
 
 
 
